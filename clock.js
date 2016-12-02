559df675cddba35cd0b24c5703992a7142a2cabe
crateros <dsvene@gmail.com>
2016-12-01 23:49:47 -0800

    Add digital readout

--- clock.js
+++ clock.js
@@ -1,58 +1,45 @@
 
+document.addEventListener("DOMContentLoaded", function(event) {
 var seconds = 0;
+var timerSeconds;
 var minutes = 0;
+var timerMinutes;
 var hours = 0;
+var timerHours;
+var digitalS = document.getElementById("digiSeconds");
+var digitalM = document.getElementById("digiMinutes");
+var digitalH = document.getElementById("digiHours");
 
 //Increment seconds, minutes, and hours each time secondsTimer fires, target DOM element, perform transform
 function clockHands() {
   seconds += 6;
-
+  digitalS.innerHTML = seconds / 6;
   var sec = document.getElementById("second");
   var min = document.getElementById("minute");
   var hour = document.getElementById("hour");
   sec.style.transform = "rotate(" + seconds + "deg)";
-  console.log('seconds:', seconds);
-  if(seconds > 360) {
+
+  if(seconds >= 361) {
     minutes += 6;
+    digitalM.innerHTML = minutes / 6 + " :";
     min.style.transform = "rotate(" + minutes + "deg)";
-    console.log('minutes:', minutes);
     seconds = 6;
+    digitalS.innerHTML = 1;
   }
-  if(minutes > 360) {
+  if(minutes >= 361) {
     hours += 30;
+    digitalH.innerHTML = hours / 30 + " :";
     hour.style.transform = "rotate(" + hours + "deg)";
-    console.log('hours:', hours);
     minutes = 6;
+    digitalM.innerHTML = 1 + " :";
   }
-  if(hours > 360) {
-    hours = 30;
+  if(hours >= 721) {
+    hours = 0;
+      digitalH.innerHTML = 0 + " :";
   }
 }
 
 //Call secondsHand function every 100ms
-var secondsTimer = setInterval(clockHands, 1000);
-//
-// //Increment minutes each time minutesTimer fires, target DOM element, perform transform
-// function minutesHand() {
-//   min.style.transform = "rotate(" + minutes + "deg)";
-//   if(minutes > 360) {
-//     minutes = 6;
-//   }
-//
-// }
-//
-// //Call minutesHand function every 600ms
-// var minutesTimer = setInterval(minutesHand, 600);
-//
-// //Increment hours each time minutesTimer fires, target DOM element, perform transform
-// function hoursHand() {
-//   hours += 30;
-//   var hour = document.getElementById("hour");
-//   hour.style.transform = "rotate(" + hours + "deg)";
-//   if(hours > 360) {
-//     hours = 30;
-//   }
-//
-// }
-// //Call hoursHand function every 3600ms
-// var hoursTimer = setInterval(hoursHand, 36000);
+var secondsTimer = setInterval(clockHands, 10);
+
+});
