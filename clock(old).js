08ea6c606fb8c9f1bc2c75b4a0ffafdbd067e4f5
crateros <dsvene@gmail.com>
2016-12-01 21:47:28 -0800

    DRY javascript

---
+++ clock(old).js
@@ -0,0 +1,45 @@
+
+var seconds = 0;
+var minutes = 0;
+var hours = 0;
+
+//Increment seconds each time secondsTimer fires, target DOM element, perform transform
+function secondsHand() {
+  seconds += 6;
+  var sec = document.getElementById("second");
+  sec.style.transform = "rotate(" + seconds + "deg)";
+  if(seconds > 360) {
+    seconds = 6;
+  }
+  console.log('seconds:', seconds);
+}
+
+//Call secondsHand function every 100ms
+var secondsTimer = setInterval(secondsHand, 10);
+
+//Increment minutes each time minutesTimer fires, target DOM element, perform transform
+function minutesHand() {
+  minutes += 6;
+  var min = document.getElementById("minute");
+  min.style.transform = "rotate(" + minutes + "deg)";
+  if(minutes > 360) {
+    minutes = 6;
+  }
+  console.log('minutes:', minutes);
+}
+
+//Call minutesHand function every 600ms
+var minutesTimer = setInterval(minutesHand, 600);
+
+//Increment hours each time minutesTimer fires, target DOM element, perform transform
+function hoursHand() {
+  hours += 30;
+  var hour = document.getElementById("hour");
+  hour.style.transform = "rotate(" + hours + "deg)";
+  if(hours > 360) {
+    hours = 30;
+  }
+  console.log('hours: ', hours);
+}
+//Call hoursHand function every 3600ms
+var hoursTimer = setInterval(hoursHand, 36000);
--- clock.js
+++ clock.js
@@ -3,43 +3,56 @@ var seconds = 0;
 var minutes = 0;
 var hours = 0;
 
-//Increment seconds each time secondsTimer fires, target DOM element, perform transform
-function secondsHand() {
+//Increment seconds, minutes, and hours each time secondsTimer fires, target DOM element, perform transform
+function clockHands() {
   seconds += 6;
+
   var sec = document.getElementById("second");
+  var min = document.getElementById("minute");
+  var hour = document.getElementById("hour");
   sec.style.transform = "rotate(" + seconds + "deg)";
+  console.log('seconds:', seconds);
   if(seconds > 360) {
+    minutes += 6;
+    min.style.transform = "rotate(" + minutes + "deg)";
+    console.log('minutes:', minutes);
     seconds = 6;
   }
-  console.log('seconds:', seconds);
-}
-
-//Call secondsHand function every 100ms
-var secondsTimer = setInterval(secondsHand, 100);
-
-//Increment minutes each time minutesTimer fires, target DOM element, perform transform
-function minutesHand() {
-  minutes += 6;
-  var min = document.getElementById("minute");
-  min.style.transform = "rotate(" + minutes + "deg)";
   if(minutes > 360) {
+    hours += 30;
+    hour.style.transform = "rotate(" + hours + "deg)";
+    console.log('hours:', hours);
     minutes = 6;
   }
-  console.log('minutes:', minutes);
-}
-
-//Call minutesHand function every 600ms
-var minutesTimer = setInterval(minutesHand, 6000);
-
-//Increment hours each time minutesTimer fires, target DOM element, perform transform
-function hoursHand() {
-  hours += 30;
-  var hour = document.getElementById("hour");
-  hour.style.transform = "rotate(" + hours + "deg)";
   if(hours > 360) {
     hours = 30;
   }
-  console.log('hours: ', hours);
 }
-//Call hoursHand function every 3600ms
-var hoursTimer = setInterval(hoursHand, 36000);
+
+//Call secondsHand function every 100ms
+var secondsTimer = setInterval(clockHands, 1000);
+//
+// //Increment minutes each time minutesTimer fires, target DOM element, perform transform
+// function minutesHand() {
+//   min.style.transform = "rotate(" + minutes + "deg)";
+//   if(minutes > 360) {
+//     minutes = 6;
+//   }
+//
+// }
+//
+// //Call minutesHand function every 600ms
+// var minutesTimer = setInterval(minutesHand, 600);
+//
+// //Increment hours each time minutesTimer fires, target DOM element, perform transform
+// function hoursHand() {
+//   hours += 30;
+//   var hour = document.getElementById("hour");
+//   hour.style.transform = "rotate(" + hours + "deg)";
+//   if(hours > 360) {
+//     hours = 30;
+//   }
+//
+// }
+// //Call hoursHand function every 3600ms
+// var hoursTimer = setInterval(hoursHand, 36000);
